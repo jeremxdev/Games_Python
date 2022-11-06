adad23219534fe8663eb2abf03193e3f5c8d660a
jeremxdev <jeremx.dev@gmail.com>
2022-11-06 18:27:36 +0100

    Jeux Python n°1

---
+++ mysteriousNumber.py
@@ -0,0 +1,52 @@
+# coding:utf-8
+
+print("""
+-----------------------------
+| Trouver le numéro mystère |
+|       Par jeremx.dev      |
+-----------------------------
+""")
+
+import random
+
+mysteriousNumber = random.randrange(0, 50)
+
+print(mysteriousNumber)
+
+game = True
+
+print("Bienvenue sur le jeu du numéro mystère !")
+
+while game:
+
+    try:
+        askToTheMysteriousNumber = input("Quel est le numéro mystère ? (compris entre 0 et 50) : ")
+        askToTheMysteriousNumber = int(askToTheMysteriousNumber)
+    except ValueError:
+        print("Tu dois rentrer un nombre entier pour le jeu !")
+        continue
+
+    if askToTheMysteriousNumber < 0:
+        print("Tu dois entrer un numéro compris entre 0 et 50 !")
+        continue
+    elif askToTheMysteriousNumber > 50:
+        print("Tu dois entrer un numéro entre 0 et 50 !")
+        continue
+    else:
+        if askToTheMysteriousNumber != mysteriousNumber:
+            print("Dommage ce n'est pas le bon numéro !")
+            asKToContinue = input("Veux-tu quand-même continuer à jouer ? (oui ou non) : ")
+            if asKToContinue == "oui":
+                print("D'accord")
+            elif asKToContinue == "non":
+                print("D'accord. Au-revoir!")
+                break
+        elif askToTheMysteriousNumber == mysteriousNumber:
+            print("Bravo tu as trouvé le bon numéro !")
+            asKToContinue = input("Veux-tu continuer à jouer ? (oui ou non) : ")
+            if asKToContinue == "oui":
+                print("D'accord")
+                continue
+            elif asKToContinue == "non":
+                print("D'accord. Au-revoir!")
+                break
