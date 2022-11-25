e69482016cc23017689b3be3971b2495fb6f29a1
Imaque Jérémy <jeremx.dev@gmail.com>
2022-11-25 21:44:52 +0100

    Jeu du pierre, feuille, scissos

---
+++ rockPaperScissors.py
@@ -0,0 +1,40 @@
+#coding:utf-8
+
+print("""
+-----------------------------------
+| Jeu de pierre, feuille, scissos |
+|       par jeremx.dev            |
+-----------------------------------
+""")
+
+from random import *
+
+tools = ["pierre", "feuille", "scissos"]
+
+def Game():
+    game = True
+    while game:
+        computerChoice = choice(tools)
+        print(computerChoice)
+        userChoice = input("pierre ?, feuille ?, scissos ? : ")
+
+        if computerChoice == "pierre" and userChoice == "feuille":
+            print("Tu as gagné ! La feuille couvre la pierre !")
+        elif computerChoice == "feuille" and userChoice == "scissos":
+            print("Tu as gagné ! Les scissos coupe la feuille !")
+        elif computerChoice == "scissos" and userChoice == "pierre":
+            print("Tu as gagné ! La pierre case les scissos !")
+        elif computerChoice == "feuille" and userChoice == "pierre":
+            print("Tu as perdu ! La feuille couvre la pierre !")
+        elif computerChoice == "pierre" and userChoice == "scissos":
+            print("Tu as perdu ! La pierre case les scissos !")
+        elif computerChoice == "feuille" and userChoice == "pierre":
+            print("Tu as perdu ! La feuille couvre la pierre !")
+        elif computerChoice == "scissos" and userChoice == "feuille":
+            print("Tu as perdu ! Les scissos coupe la feuille !")
+        elif computerChoice == userChoice:
+            print("Dommage, égalité !")
+        else:
+            break
+
+Game()  
