c5b677a8a815d544df84a6f9e10b62cbed11d64b
jeremxdev <imaquejeremy@gmail.com>
2022-11-30 12:09:53 +0100

    Tu peux télécharger des vidéos avec ce code Python!

---
+++ YouTubeDownloader.py
@@ -0,0 +1,12 @@
+from pytube import YouTube
+
+def Download(link):
+    YouTubeObject = YouTube(link)
+    YouTubeObject = YouTubeObject.streams.get_highest_resolution()
+    try:
+        YouTubeObject.download()
+    except:
+        print("There has been an error in downloading your youtube video")
+    print("This download has completed Yes !!")
+
+    link = input("Put your youtube link here!! URL: ")
