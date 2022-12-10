6bc15f56757e5242188b3ff27c6af7c71a10be45
jeremxdev <imaquejeremy@gmail.com>
2022-12-10 18:11:13 +0100

    Console en Python ;-)

--- console.py
+++ console.py
@@ -21,34 +21,41 @@ def hello():
 def operation():
     operator = input("Choisir un opérateur (+ ou - ou * ou /) : ")
 
-    if operator == "+":
-        print("Bien, vous avez choisis l'addition!") 
-        operanteOne = input("Choisir un nombre : ")
-        operanteOne = int(operanteOne)
-        operanteTwo = input("Choisir un deuxième nombre : ")
-        operanteTwo = int(operanteTwo)
-        print(f"Le résultat est : {operanteOne + operanteTwo}")
-    elif operator == "-":
-        print("Bien, vous avez choisis la soustraction!")
-        operanteOne = input("Choisir un nombre : ")
-        operanteOne = int(operanteOne)
-        operanteTwo = input("Choisir un deuxxième nombre : ")
-        operanteTwo = int(operanteTwo)
-        print(f"Le résultat est : {operanteOne - operanteTwo}")
-    elif operator == "*":
-        print("Bien, vous avez choisis la multiplication!")
-        operanteOne = input("Choisir un nombre : ")
-        operanteOne = int(operanteOne)
-        operanteTwo = input("Choisir un deuxième nombre : ")
-        operanteTwo = int(operanteTwo)
-        print(f"Le résultat est : {operanteOne * operanteTwo}")
-    elif operator == "/":
-        print("Bien, vous avez choisis la divison!")
-        operanteOne = input("Choisir un nombre : ")
-        operanteOne = int(operanteOne)
-        operanteTwo = input("Choisir un deuxième nombre : ")
-        operanteTwo = int(operanteTwo)
-        print(f"Le résultat est : {operanteOne / operanteTwo}")
+    try:
+        if operator == "+":
+            print("Bien, vous avez choisis l'addition!") 
+            operanteOne = input("Choisir un nombre : ")
+            operanteOne = int(operanteOne)
+            operanteTwo = input("Choisir un deuxième nombre : ")
+            operanteTwo = int(operanteTwo)
+            print(f"Le résultat est : {operanteOne + operanteTwo}")
+        elif operator == "-":
+            print("Bien, vous avez choisis la soustraction!")
+            operanteOne = input("Choisir un nombre : ")
+            operanteOne = int(operanteOne)
+            operanteTwo = input("Choisir un deuxxième nombre : ")
+            operanteTwo = int(operanteTwo)
+            print(f"Le résultat est : {operanteOne - operanteTwo}")
+        elif operator == "*":
+            print("Bien, vous avez choisis la multiplication!")
+            operanteOne = input("Choisir un nombre : ")
+            operanteOne = int(operanteOne)
+            operanteTwo = input("Choisir un deuxième nombre : ")
+            operanteTwo = int(operanteTwo)
+            print(f"Le résultat est : {operanteOne * operanteTwo}")
+        elif operator == "/":
+            print("Bien, vous avez choisis la divison!")
+            operanteOne = input("Choisir un nombre : ")
+            operanteOne = int(operanteOne)
+            operanteTwo = input("Choisir un deuxième nombre : ")
+            operanteTwo = int(operanteTwo)
+            print(f"Le résultat est : {operanteOne / operanteTwo}")
+    except ValueError:
+        print("Erreur de la valeur saisie!")
+    except TypeError:
+        print("Erreur du type de la saisie")
+    except:
+        print("Error générale")
 
 while True:
     commandPrompt = input("Choisir une commande (help... par exemple) : ")
